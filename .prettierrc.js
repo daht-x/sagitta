69cfbdbc09d2be5c8315003db05368ba26fda375
David Hernández <daht.x0@gmail.com>
2023-10-05 00:15:53 -0500

    chore: add prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+const rootConfiguration = {
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+	trailingComma: "none",
+	arrowParens: "avoid",
+	endOfLine: "crlf"
+};
+
+export default rootConfiguration;
