3b4c57cffa35a5f77bce927b1e76eb071ad26e2e
David Hernández <daht.x0@gmail.com>
2023-10-05 22:56:50 -0500

    chore: add lint-staged configuration

---
+++ .lintstagedrc.js
@@ -0,0 +1,16 @@
+import path from "path";
+
+const rootConfiguration = {
+	"**.md": ["markdownlint --fix", 'markdown-link-check --progress --config ".markdownlc.json"'],
+	"**.js": ["prettier --write", "eslint --fix"],
+	"**.cs": absolutePaths => {
+		const currentWorkingDirectory = process.cwd();
+		const relativePaths = absolutePaths
+			.map(absolutePath => path.relative(currentWorkingDirectory, absolutePath))
+			.join(" ");
+		const command = `dotnet format --include ${relativePaths}`;
+		return command;
+	}
+};
+
+export default rootConfiguration;
