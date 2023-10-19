732940859855a12010d3a079f3303618d6d4e55d
David Hernández <daht.x0@gmail.com>
2023-10-18 19:01:08 -0500

    style: apply `ESLint` rules

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,8 @@ import path from "path";
 const rootConfiguration = {
 	"**.md": ["markdownlint --fix", "markdown-link-check --progress --config \".markdownlc.json\""],
 	"**.js": "eslint --fix",
-	"**/*.cs": absolutePaths => {
+	"**/*.cs": absolutePaths =>
+	{
 		const currentWorkingDirectory = process.cwd();
 		const relativePaths = absolutePaths
 			.map(absolutePath => path.relative(currentWorkingDirectory, absolutePath))
