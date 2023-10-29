7baba57dae1c1c55c7d7dbf7bde637d0ba300a44
David Hernández <140873695+daht-x@users.noreply.github.com>
2023-10-29 16:00:53 -0500

    chore: remove explicit conversion of `C#` relative paths

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,17 +1,7 @@
-import path from "path";
-
 const rootConfiguration = {
 	"**.md": ["markdownlint --fix", "markdown-link-check --progress --config '.markdownlc.json'"],
 	"**.js": "eslint --fix",
-	"**/*.cs": absolutePaths =>
-	{
-		const currentWorkingDirectory = process.cwd();
-		const relativePaths = absolutePaths
-			.map(absolutePath => path.relative(currentWorkingDirectory, absolutePath))
-			.join(" ");
-		const command = `dotnet format --include ${relativePaths}`;
-		return command;
-	}
+	"**/*.cs": "dotnet format --include"
 };
 
 export default rootConfiguration;
