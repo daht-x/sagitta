4bef1c4453c48668821ddba141c20882f88b6e1a
David Hernández <140873695+daht-x@users.noreply.github.com>
2023-12-02 20:00:58 -0500

    feature: add `Fail` method

--- test/unit/Monads/Asserters/ResultAsserter.cs
+++ test/unit/Monads/Asserters/ResultAsserter.cs
@@ -2,6 +2,19 @@ namespace Daht.Sagitta.Core.UnitTest.Monads.Asserters;
 
 internal static class ResultAsserter
 {
+	internal static void AreFailed<TFailure, TSuccess>(TFailure expectedFailure, Result<TFailure, TSuccess> actualResult)
+		where TFailure : notnull
+		where TSuccess : notnull
+	{
+		Assert.True(actualResult.IsFailedOrDefault);
+		Assert.False(actualResult.IsSuccessfulOrDefault);
+		Assert.False(actualResult.IsDefault);
+		Assert.True(actualResult.IsFailed);
+		Assert.Equal(expectedFailure, actualResult.Failure);
+		Assert.False(actualResult.IsSuccessful);
+		Assert.Equal(default, actualResult.Success);
+	}
+
 	internal static void AreSuccessful<TFailure, TSuccess>(TSuccess expectedSuccess, Result<TFailure, TSuccess> actualResult)
 		where TFailure : notnull
 		where TSuccess : notnull
