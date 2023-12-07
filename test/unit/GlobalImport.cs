813553eacc834472abcfec49122f232f898ab0d6
David Hernández <daht.x0@gmail.com>
2023-12-06 20:55:56 -0500

    test: replace `TSuccess` from `string` to `Constellation`

--- test/unit/GlobalImport.cs
+++ test/unit/GlobalImport.cs
@@ -3,5 +3,6 @@ global using Daht.Sagitta.Core.UnitTest.Monads.Asserters;
 global using Daht.Sagitta.Core.UnitTest.Monads.Fixtures;
 global using Daht.Sagitta.Core.UnitTest.Shared.Exceptions;
 global using Daht.Sagitta.Core.UnitTest.Shared.Exceptions.Asserters;
+global using Daht.Sagitta.Core.UnitTest.Shared.Fakers;
 
 global using Xunit;
