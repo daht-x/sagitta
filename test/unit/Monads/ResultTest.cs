0c6caf831c77aa1b85b5284e222eac15aa7f8b76
David Hernández <daht.x0@gmail.com>
2023-12-17 19:42:50 -0500

    feature: remove all `ArgumentNullException` exceptions

--- test/unit/Monads/ResultTest.cs
+++ test/unit/Monads/ResultTest.cs
@@ -12,26 +12,6 @@ public sealed class ResultTest
 
 	[Fact]
 	[Trait(root, implicitOperator)]
-	public void ImplicitOperator_NullSuccess_ArgumentNullException()
-	{
-		//Arrange
-		const Constellation success = null!;
-
-		//Act
-		ArgumentNullException? actualException = ExceptionHandler.Catch<ArgumentNullException>(
-			static () =>
-			{
-#pragma warning disable S1481
-				Result<Constellation, string> _ = success;
-#pragma warning restore S1481
-			}
-		);
-		//Assert
-		ArgumentNullExceptionAsserter.AreEqualParameterNames(nameof(success), actualException);
-	}
-
-	[Fact]
-	[Trait(root, implicitOperator)]
 	public void ImplicitOperator_Success_SuccessfulResult()
 	{
 		//Arrange
@@ -47,27 +27,6 @@ public sealed class ResultTest
 	#endregion
 
 	#region Overload No. 02
-
-	[Fact]
-	[Trait(root, implicitOperator)]
-	public void ImplicitOperator_NullFailure_ArgumentNullException()
-	{
-		//Arrange
-		const string failure = null!;
-
-		//Act
-		ArgumentNullException? actualException = ExceptionHandler.Catch<ArgumentNullException>(
-			static () =>
-			{
-#pragma warning disable S1481
-				Result<Constellation, string> _ = failure;
-#pragma warning restore S1481
-			}
-		);
-
-		//Assert
-		ArgumentNullExceptionAsserter.AreEqualParameterNames(nameof(failure), actualException);
-	}
 
 	[Fact]
 	[Trait(root, implicitOperator)]
