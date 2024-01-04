0774d0e0021990e77d60d1a3d1185b5b503fdb32
David Hernández <140873695+daht-x@users.noreply.github.com>
2024-01-04 01:06:00 -0500

    feature(`Result`): add overload of the `Ensure` method

--- test/unit/Monads/ResultTest.cs
+++ test/unit/Monads/ResultTest.cs
@@ -92,6 +92,8 @@ public sealed class ResultTest
 
 	#region Ensure
 
+	#region Overload
+
 	[Fact]
 	[Trait(root, ensure)]
 	public void Ensure_FailedResultPlusTruePredicatePlusFailure_FailedResult()
@@ -141,6 +143,63 @@ public sealed class ResultTest
 		// Assert
 		ResultAsserter.AreSuccessful(expectedSuccess, actualResult);
 	}
+
+	#endregion
+
+	#region Overload
+
+	[Fact]
+	[Trait(root, ensure)]
+	public void Ensure_FailedResultPlusTruePredicatePlusCreateFailure_FailedResult()
+	{
+		// Arrange
+		const string expectedFailure = ResultFixture.Failure;
+		Func<Constellation, bool> predicate = static _ => true;
+		Func<Constellation, string> createFailure = static _ => ResultFixture.RandomFailure;
+
+		// Act
+		Result<Constellation, string> actualResult = ResultMother.Fail(expectedFailure)
+			.Ensure(predicate, createFailure);
+
+		// Assert
+		ResultAsserter.AreFailed(expectedFailure, actualResult);
+	}
+
+	[Fact]
+	[Trait(root, ensure)]
+	public void Ensure_SuccessfulResultPlusTruePredicatePlusCreateFailure_FailedResult()
+	{
+		// Arrange
+		Func<Constellation, bool> predicate = static _ => true;
+		const string expectedFailure = ResultFixture.Failure;
+		Func<Constellation, string> createFailure = static _ => expectedFailure;
+
+		// Act
+		Result<Constellation, string> actualResult = ResultMother.Succeed()
+			.Ensure(predicate, createFailure);
+
+		// Assert
+		ResultAsserter.AreFailed(expectedFailure, actualResult);
+	}
+
+	[Fact]
+	[Trait(root, ensure)]
+	public void Ensure_SuccessfulResultPlusFalsePredicatePlusCreateFailure_SuccessfulResult()
+	{
+		// Arrange
+		Constellation expectedSuccess = ResultFixture.Success;
+		Func<Constellation, bool> predicate = static _ => false;
+		Func<Constellation, string> createFailure = static _ => ResultFixture.Failure;
+
+		// Act
+		Result<Constellation, string> actualResult = ResultMother.Succeed(expectedSuccess)
+			.Ensure(predicate, createFailure);
+
+		// Assert
+		ResultAsserter.AreSuccessful(expectedSuccess, actualResult);
+	}
+
+	#endregion
 
 	#endregion
 }
