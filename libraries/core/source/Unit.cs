66909f057f3513ed8514e0cc2656be15e816a5ba
David Hernández <140873695+daht-x@users.noreply.github.com>
2025-08-09 22:24:02 -0500

    compatibility(`Unit`): remove automatically generated `record struct` features

--- libraries/core/source/Unit.cs
+++ libraries/core/source/Unit.cs
@@ -6,9 +6,51 @@
 namespace Daht.Sagitta.Core;
 
 /// <summary>Represents the absence of a specific value, explicitly simulating the <see langword="void"/> type.</summary>
-public readonly record struct Unit
+public readonly struct Unit : IEquatable<Unit>
 {
-	/// <summary>The placeholder that indicates the discarding of a specific value.</summary>
+	/// <summary>The placeholder that indicates the discarding of a value.</summary>
 	public static Unit Default
 		=> default;
+
+	/// <summary>Determines whether the left unit is not equal to the right unit.</summary>
+	/// <param name="left">The main unit.</param>
+	/// <param name="right">The unit to compare.</param>
+	/// <returns><see langword="true" /> if the left unit is not equal to the right unit; otherwise, <see langword="false" />.</returns>
+	[Pure]
+	public static bool operator !=(Unit left, Unit right)
+		=> !(left == right);
+
+	/// <summary>Determines whether the left unit is equal to the right unit.</summary>
+	/// <param name="left">The main unit.</param>
+	/// <param name="right">The unit to compare.</param>
+	/// <returns><see langword="true" /> if the left unit is equal to the right unit; otherwise, <see langword="false" />.</returns>
+	[Pure]
+	public static bool operator ==(Unit left, Unit right)
+		=> left.Equals(right);
+
+	/// <summary>Determines whether the specified unit is equal to the current unit.</summary>
+	/// <param name="obj">The unit to compare with the current reference.</param>
+	/// <returns><see langword="true" /> if the specified unit is equal to the current unit; otherwise, <see langword="false" />.</returns>
+	[Pure]
+	public override bool Equals(object? obj)
+		=> obj is Unit;
+
+	/// <summary>Determines whether the specified unit is equal to the current unit.</summary>
+	/// <param name="other">The unit to compare with the current reference.</param>
+	/// <returns><see langword="true" /> if the specified unit is equal to the current unit; otherwise, <see langword="false" />.</returns>
+	[Pure]
+	public bool Equals(Unit other)
+		=> true;
+
+	/// <summary>Gets the hash code of the current unit.</summary>
+	/// <returns>The hash code of the current unit.</returns>
+	[Pure]
+	public override int GetHashCode()
+		=> 0;
+
+	/// <summary>Gets the value of the current unit.</summary>
+	/// <returns>The value of the current unit.</returns>
+	[Pure]
+	public override string ToString()
+		=> string.Empty;
 }
