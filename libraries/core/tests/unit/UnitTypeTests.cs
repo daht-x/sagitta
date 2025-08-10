bb60c282eb6e4fbab517031f8064e74c787ead6b
David Hernández <daht.x0@gmail.com>
2025-08-09 22:07:15 -0500

    compatibility(`Unit`): remove automatically generated `record struct` features

---
+++ libraries/core/tests/unit/UnitTypeTests.cs
@@ -0,0 +1,216 @@
+// ----------------------------------------------------------------------------------------------------------
+// Copyright (c) David Andrés Hernández Triana. All rights reserved.
+// Licensed under the MIT License. Please refer to the license file in the project root for more information.
+// ----------------------------------------------------------------------------------------------------------
+
+namespace Daht.Sagitta.Core.UnitTests;
+
+public sealed class UnitTypeTests
+{
+	private const string @base = nameof(Unit);
+
+	private const string memberEqualOperator = "==";
+
+	private const string memberNotEqualOperator = "!=";
+
+	private const string memberEquals = nameof(Unit.Equals);
+
+	private const string memberGetHashCode = nameof(Unit.GetHashCode);
+
+	private const string memberToString = nameof(Unit.ToString);
+
+	#region ==
+
+	[Fact]
+	[Trait(@base, memberEqualOperator)]
+	[SuppressMessage(
+		MaintainabilityAnalysisCategory.Name,
+		MaintainabilityAnalysisCategory.Rules.AvoidDeadConditionalCode
+	)]
+	public void EqualOperator_DefaultLeftAndNullRight_False()
+	{
+		Unit left = Unit.Default;
+		Unit? right = null;
+		bool actual = left == right;
+		Assert.False(actual);
+	}
+
+	[Fact]
+	[Trait(@base, memberEqualOperator)]
+	[SuppressMessage(
+		MaintainabilityAnalysisCategory.Name,
+		MaintainabilityAnalysisCategory.Rules.AvoidDeadConditionalCode
+	)]
+	public void EqualOperator_NullLeftAndDefaultRight_False()
+	{
+		Unit? left = null;
+		Unit right = Unit.Default;
+		bool actual = left == right;
+		Assert.False(actual);
+	}
+
+	[Fact]
+	[Trait(@base, memberEqualOperator)]
+	[SuppressMessage(
+		MaintainabilityAnalysisCategory.Name,
+		MaintainabilityAnalysisCategory.Rules.AvoidDeadConditionalCode
+	)]
+	public void EqualOperator_NullLeftAndNullRight_True()
+	{
+		Unit? left = null;
+		Unit? right = null;
+		bool actual = left == right;
+		Assert.True(actual);
+	}
+
+	[Fact]
+	[Trait(@base, memberEqualOperator)]
+	public void EqualOperator_DefaultLeftAndDefaultRight_True()
+	{
+		Unit left = Unit.Default;
+		Unit right = Unit.Default;
+		bool actual = left == right;
+		Assert.True(actual);
+	}
+
+	#endregion ==
+
+	#region !=
+
+	[Fact]
+	[Trait(@base, memberNotEqualOperator)]
+	public void NotEqualOperator_DefaultLeftAndDefaultRight_False()
+	{
+		Unit left = Unit.Default;
+		Unit right = Unit.Default;
+		bool actual = left != right;
+		Assert.False(actual);
+	}
+
+	[Fact]
+	[Trait(@base, memberNotEqualOperator)]
+	[SuppressMessage(
+		MaintainabilityAnalysisCategory.Name,
+		MaintainabilityAnalysisCategory.Rules.AvoidDeadConditionalCode
+	)]
+	public void NotEqualOperator_NullLeftAndNullRight_False()
+	{
+		Unit? left = null;
+		Unit? right = null;
+		bool actual = left != right;
+		Assert.False(actual);
+	}
+
+	[Fact]
+	[Trait(@base, memberNotEqualOperator)]
+	[SuppressMessage(
+		MaintainabilityAnalysisCategory.Name,
+		MaintainabilityAnalysisCategory.Rules.AvoidDeadConditionalCode
+	)]
+	public void NotEqualOperator_NullLeftAndDefaultRight_True()
+	{
+		Unit? left = null;
+		Unit right = Unit.Default;
+		bool actual = left != right;
+		Assert.True(actual);
+	}
+
+	[Fact]
+	[Trait(@base, memberNotEqualOperator)]
+	[SuppressMessage(
+		MaintainabilityAnalysisCategory.Name,
+		MaintainabilityAnalysisCategory.Rules.AvoidDeadConditionalCode
+	)]
+	public void NotEqualOperator_DefaultLeftAndNullRight_True()
+	{
+		Unit left = Unit.Default;
+		Unit? right = null;
+		bool actual = left != right;
+		Assert.True(actual);
+	}
+
+	#endregion !=
+
+	#region Equals
+
+	#region Equals overload
+
+	[Fact]
+	[Trait(@base, memberEquals)]
+	[SuppressMessage(
+		MaintainabilityAnalysisCategory.Name,
+		MaintainabilityAnalysisCategory.Rules.AvoidDeadConditionalCode
+	)]
+	public void Equals_DefaultUnitAndNullObject_False()
+	{
+		Unit left = Unit.Default;
+		object? right = null;
+		bool actual = left.Equals(right);
+		Assert.False(actual);
+	}
+
+	[Fact]
+	[Trait(@base, memberEquals)]
+	public void Equals_DefaultUnitAndDifferentObject_False()
+	{
+		Unit left = Unit.Default;
+		object right = string.Empty;
+		bool actual = left.Equals(right);
+		Assert.False(actual);
+	}
+
+	[Fact]
+	[Trait(@base, memberEquals)]
+	public void Equals_DefaultUnitAndDefaultUnitObject_True()
+	{
+		Unit left = Unit.Default;
+		object right = Unit.Default;
+		bool actual = left.Equals(right);
+		Assert.True(actual);
+	}
+
+	#endregion Equals overload
+
+	#region Equals overload
+
+	[Fact]
+	[Trait(@base, memberEquals)]
+	public void Equals_DefaultUnitAndDefaultUnit_True()
+	{
+		Unit left = Unit.Default;
+		Unit right = Unit.Default;
+		bool actual = left.Equals(right);
+		Assert.True(actual);
+	}
+
+	#endregion Equals overload
+
+	#endregion Equals
+
+	#region GetHashCode
+
+	[Fact]
+	[Trait(@base, memberGetHashCode)]
+	public void GetHashCode_DefaultUnit_Zero()
+	{
+		const int expected = 0;
+		int actual = Unit.Default
+			.GetHashCode();
+		Assert.Equal(expected, actual);
+	}
+
+	#endregion GetHashCode
+
+	#region ToString
+
+	[Fact]
+	[Trait(@base, memberToString)]
+	public void ToString_DefaultUnit_Empty()
+	{
+		Unit unit = Unit.Default;
+		string actual = unit.ToString();
+		Assert.Empty(actual);
+	}
+
+	#endregion ToString
+}
