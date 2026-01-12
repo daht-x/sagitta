d53a820b7051e8fc7cb230ba625eedea36b36d91
David Hernández <daht.x0@gmail.com>
2026-01-12 18:22:42 -0500

    chore: enable strict linter rules and assist actions

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,52 +1,56 @@
 import type { UserConfig } from "@commitlint/types";
 import { RuleConfigSeverity } from "@commitlint/types";
 
-enum RuleCompliance {
-	Always = "always",
-	Never = "never"
-}
+const ruleCompliance = {
+	always: "always",
+	never: "never"
+} as const;
 const fullStop = ".";
 const defaultMaximumLength = 120;
 const defaultMaximumLineLength = 300;
 const defaultMinimumLength = 0;
-enum NamingStyle {
-	LowerCase = "lower-case"
-}
+const namingStyle = {
+	lowerCase: "lower-case"
+} as const;
 const signatureLabel = "Signed-off-by:";
 const rootConfiguration: UserConfig = {
 	rules: {
-		"body-full-stop": [RuleConfigSeverity.Error, RuleCompliance.Always, fullStop],
-		"body-leading-blank": [RuleConfigSeverity.Error, RuleCompliance.Always],
-		"body-empty": [RuleConfigSeverity.Warning, RuleCompliance.Never],
-		"body-max-length": [RuleConfigSeverity.Error, RuleCompliance.Always, Number.POSITIVE_INFINITY],
-		"body-max-line-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMaximumLineLength],
-		"body-min-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMinimumLength],
 		"body-case": [RuleConfigSeverity.Disabled],
-		"footer-leading-blank": [RuleConfigSeverity.Error, RuleCompliance.Always],
-		"footer-empty": [RuleConfigSeverity.Warning, RuleCompliance.Never],
-		"footer-max-length": [RuleConfigSeverity.Error, RuleCompliance.Always, Number.POSITIVE_INFINITY],
-		"footer-max-line-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMaximumLineLength],
-		"footer-min-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMinimumLength],
-		"header-trim": [RuleConfigSeverity.Error, RuleCompliance.Always],
-		"header-case": [RuleConfigSeverity.Warning, RuleCompliance.Always, NamingStyle.LowerCase],
-		"header-full-stop": [RuleConfigSeverity.Error, RuleCompliance.Never, fullStop],
-		"header-max-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMaximumLength],
-		"header-min-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMinimumLength],
-		"references-empty": [RuleConfigSeverity.Warning, RuleCompliance.Never],
+		"body-empty": [RuleConfigSeverity.Warning, ruleCompliance.never],
+		"body-full-stop": [RuleConfigSeverity.Error, ruleCompliance.always, fullStop],
+		"body-leading-blank": [RuleConfigSeverity.Error, ruleCompliance.always],
+		"body-max-length": [RuleConfigSeverity.Error, ruleCompliance.always, Number.POSITIVE_INFINITY],
+		"body-max-line-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMaximumLineLength],
+		"body-min-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMinimumLength],
+		"footer-empty": [RuleConfigSeverity.Warning, ruleCompliance.never],
+		"footer-leading-blank": [RuleConfigSeverity.Error, ruleCompliance.always],
+		"footer-max-length": [RuleConfigSeverity.Error, ruleCompliance.always, Number.POSITIVE_INFINITY],
+		"footer-max-line-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMaximumLineLength],
+		"footer-min-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMinimumLength],
+		"header-case": [RuleConfigSeverity.Warning, ruleCompliance.always, namingStyle.lowerCase],
+		"header-full-stop": [RuleConfigSeverity.Error, ruleCompliance.never, fullStop],
+		"header-max-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMaximumLength],
+		"header-min-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMinimumLength],
+		"header-trim": [RuleConfigSeverity.Error, ruleCompliance.always],
+		"references-empty": [RuleConfigSeverity.Warning, ruleCompliance.never],
+		"scope-case": [RuleConfigSeverity.Warning, ruleCompliance.always, namingStyle.lowerCase],
+		"scope-empty": [RuleConfigSeverity.Warning, ruleCompliance.never],
 		"scope-enum": [RuleConfigSeverity.Disabled],
-		"scope-case": [RuleConfigSeverity.Warning, RuleCompliance.Always, NamingStyle.LowerCase],
-		"scope-empty": [RuleConfigSeverity.Warning, RuleCompliance.Never],
-		"scope-max-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMaximumLength],
-		"scope-min-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMinimumLength],
-		"subject-case": [RuleConfigSeverity.Warning, RuleCompliance.Always, NamingStyle.LowerCase],
-		"subject-empty": [RuleConfigSeverity.Error, RuleCompliance.Never],
-		"subject-full-stop": [RuleConfigSeverity.Error, RuleCompliance.Never, fullStop],
-		"subject-max-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMaximumLength],
-		"subject-min-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMinimumLength],
-		"subject-exclamation-mark": [RuleConfigSeverity.Error, RuleCompliance.Never],
+		"scope-max-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMaximumLength],
+		"scope-min-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMinimumLength],
+		"signed-off-by": [RuleConfigSeverity.Warning, ruleCompliance.always, signatureLabel],
+		"subject-case": [RuleConfigSeverity.Warning, ruleCompliance.always, namingStyle.lowerCase],
+		"subject-empty": [RuleConfigSeverity.Error, ruleCompliance.never],
+		"subject-exclamation-mark": [RuleConfigSeverity.Error, ruleCompliance.never],
+		"subject-full-stop": [RuleConfigSeverity.Error, ruleCompliance.never, fullStop],
+		"subject-max-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMaximumLength],
+		"subject-min-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMinimumLength],
+		"trailer-exists": [RuleConfigSeverity.Warning, ruleCompliance.always, signatureLabel],
+		"type-case": [RuleConfigSeverity.Error, ruleCompliance.always, namingStyle.lowerCase],
+		"type-empty": [RuleConfigSeverity.Error, ruleCompliance.never],
 		"type-enum": [
 			RuleConfigSeverity.Error,
-			RuleCompliance.Always,
+			ruleCompliance.always,
 			[
 				"compatibility",
 				"feature",
@@ -61,12 +65,8 @@ const rootConfiguration: UserConfig = {
 				"workflow"
 			]
 		],
-		"type-case": [RuleConfigSeverity.Error, RuleCompliance.Always, NamingStyle.LowerCase],
-		"type-empty": [RuleConfigSeverity.Error, RuleCompliance.Never],
-		"type-max-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMaximumLength],
-		"type-min-length": [RuleConfigSeverity.Error, RuleCompliance.Always, defaultMinimumLength],
-		"signed-off-by": [RuleConfigSeverity.Warning, RuleCompliance.Always, signatureLabel],
-		"trailer-exists": [RuleConfigSeverity.Warning, RuleCompliance.Always, signatureLabel]
+		"type-max-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMaximumLength],
+		"type-min-length": [RuleConfigSeverity.Error, ruleCompliance.always, defaultMinimumLength]
 	}
 };
 
