6aaa2560a2e8ccdb869eef6678e191794bc3c370
David Hernández <daht.x0@gmail.com>
2026-01-13 23:20:20 -0500

    documentation: add base `Starlight` configuration

---
+++ astro.config.ts
@@ -0,0 +1,25 @@
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+const configuration = defineConfig({
+	integrations: [
+		starlight({
+			favicon: "./documentation/public/favicon.svg",
+			sidebar: [
+				{
+					items: [{ label: "Example Guide", slug: "guides/example" }],
+					label: "Guides"
+				},
+				{
+					autogenerate: { directory: "reference" },
+					label: "Reference"
+				}
+			],
+			social: [{ href: "https://github.com/withastro/starlight", icon: "github", label: "GitHub" }],
+			title: "My Docs"
+		})
+	],
+	srcDir: "./documentation/source"
+});
+
+export default configuration;
